6d5a5857f190917edd8ad54fb039dfee029d5377
Ives De Bruycker <ives.debruycker@gmail.com>
2016-10-30 12:56:42 +0000

    Distinct node-red-nodes for querying devices and updating temperatures. Fix sample reference in package.js Fixes #2 Fixes #4 Fixes #5

--- maxcube.js
+++ maxcube.js
@@ -1,18 +1,17 @@
 var MaxCube = require('maxcube');
 
 module.exports = function(RED) {
-  function MaxcubeNode(config) {
+  function MaxcubeNodeIn(config) {
     var node = this;
     RED.nodes.createNode(this, config);
-    var node = this;
-    this.ip = config.ip;
-    this.port = config.port;
 
-    if (!node.ip || !node.port) {
+    this.serverConfig = RED.nodes.getNode(config.server);
+
+    if (!this.serverConfig) {
       return;
     }
 
-    node.maxCube = new MaxCube(node.ip, node.port);
+    node.maxCube = this.serverConfig.maxCube;
 
     node.maxCube.on('closed', function () {
       node.status({fill:"red",shape:"ring",text:"disconnected"});
@@ -31,10 +30,54 @@ module.exports = function(RED) {
         }
       });
     });
+  }
+  RED.nodes.registerType("maxcube in", MaxcubeNodeIn);
+
+  function MaxcubeNodeOut(config) {
+    var node = this;
+    RED.nodes.createNode(this, config);
+
+    this.serverConfig = RED.nodes.getNode(config.server);
+
+    if (!this.serverConfig) {
+      return;
+    }
+
+    node.maxCube = this.serverConfig.maxCube;
+
+    node.maxCube.on('closed', function () {
+      node.status({fill:"red",shape:"ring",text:"disconnected"});
+    });
+
+    node.maxCube.on('connected', function () {
+      node.status({fill:"green",shape:"dot",text:"connected"});
+    });
+
+    node.on('input', function(msg) {
+      node.maxCube.getDeviceStatus().then(function (payload) {
+        // send devices statuses as separate messages
+        node.send([payload.map(function function_name(deviceStatus) { return { rf_address: deviceStatus.rf_address, payload: deviceStatus }; })]);
+      });
+    });
+  }
+  RED.nodes.registerType("maxcube out", MaxcubeNodeOut);
+
+  function MaxcubeServerNode(config) {
+    var node = this;
+    RED.nodes.createNode(this, config);
+
+    this.host = config.host;
+    this.port = config.port;
+
+    if (!node.host || !node.port) {
+      return;
+    }
+
+    node.maxCube = new MaxCube(node.host, node.port);
 
     node.on("close", function() {
       node.maxCube.close();
     });
   }
-  RED.nodes.registerType("maxcube", MaxcubeNode);
+  RED.nodes.registerType("maxcube-server", MaxcubeServerNode);
 }
